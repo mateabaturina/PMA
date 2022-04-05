355525d56bd116fc1c5857aef18dfb60372cd14a
mateabaturina <80029174+mateabaturina@users.noreply.github.com>
2022-04-05 10:00:05 +0200

    Add files via upload

--- Vjezba06/index.js
+++ Vjezba06/index.js
@@ -22,16 +22,16 @@
 //let br = 123;
 //console.log(`${br}\n ${typeof(br)}`);
 
-let a = 100;
-let b = 200;
-let c = "300";
+//let a = 100;
+//let b = 200;
+//let c = "300";
 
-var rez = a + b + c;
-console.log(rez);
-rez = c + a + b;
-console.log(rez);
-rez = b + c + a;
-console.log(rez);
+//var rez = a + b + c;
+//console.log(rez);
+//rez = c + a + b;
+//console.log(rez);
+//rez = b + c + a;
+//console.log(rez);
 
 //let noviniz = [1, 2, 3, 4];
 //console.log(noviniz);
@@ -42,17 +42,17 @@ console.log(rez);
 //let nniz = new Array(10, 2, 3);
 //console.log(`niz: ${niz}\n nniz: ${nniz}`);
 
-let niz1 = [1,14,'abc','d',55];
-let niz2 = niz1;
-niz2[3] = 'ABC';
-console.log(`niz1: ${niz1}\n\nniz2: ${niz2}`);
+//let niz1 = [1,14,'abc','d',55];
+//let niz2 = niz1;
+//niz2[3] = 'ABC';
+//console.log(`niz1: ${niz1}\n\nniz2: ${niz2}`);
 
 //1. Zadatak
 let niz = [1, 2, 3, 4, 5];
 
 for(let i = 0; i < niz.length; i++){
     if ((niz[i]%2) != 0){
-        console.log(`niz: ${niz[i]}`);
+        console.log(`neparni element niza: ${niz[i]}`);
     }
 };
 
@@ -66,7 +66,7 @@ console.log(`Duljina niza 1: ${niz_1.length}\n\nDuljina niza 2: ${niz_2.length}`
 let countNumber1 = 0;
 let countString1 = 0;
 for(let i = 0; i < niz_1.length; i++){
-    if (typeof(niz_1[i]) == Number){
+    if (typeof(niz_1[i]) == "number"){
         countNumber1++;
     }
     else{
@@ -79,7 +79,7 @@ console.log(`niz1 brojčani elementi: ${countNumber1}\n\nniz1 nebrojčani elemen
 let countNumber2 = 0;
 let countString2 = 0;
 for(let i = 0; i < niz_2.length; i++){
-    if (typeof(niz_2[i]) == Number){
+    if (typeof(niz_2[i]) == "number"){
         countNumber2++;
     }
     else{
@@ -88,3 +88,20 @@ for(let i = 0; i < niz_2.length; i++){
 };
 
 console.log(`niz1 brojčani elementi: ${countNumber2}\n\nniz1 nebrojčani elementi: ${countString2}`);
+
+//4. Zadatak
+niz_lower = ['hawkeye', 'black widow'];
+niz_upper = ['CAPTAIN AMERICA', 'IRON MAN', 'HULK', 'THOR'];
+
+niz_copy = niz_lower.copyWithin();
+niz_copy = niz_copy.concat(niz_upper);
+
+console.log(`kopija niza: ${niz_copy}`);
+
+//5. Zadatak
+const start = 1;
+const delete_count = 3;
+
+niz_copy.splice(start, delete_count);
+
+console.log(`kopija niza nakon uklanjanja elemenata: ${niz_copy}`); 
